faec84d1403f885052220232d7d62199979ad4aa
Douglas Barbosa <douglas.makubex@gmail.com>
2014-03-07 09:50:21 -0300

    Fix some bugs

--- termquots.sh
+++ termquots.sh
@@ -8,29 +8,36 @@
 # # quots per author
 #
 # Version-1.3 - Create functions for some options
-# Version-1.4 - Better sort and fix the err_message about pick up an option
-#				that non exist
+#
+#
 
 
-HELP_MESSAGE(){	
+
+
+HELP_MESSAGE(){
+	#echo "The name is a abbreviation of Quote of the day" 	
 	echo "Name: $(basename "$0") version-1.3"
 	echo "Usage: $(basename "$0") [ OPTIONS ]"
-	echo "	-h, --help		show this help and exit"
-	echo "	-A, --Authors		show the name of all authors"
+	echo "  -h, --help          show this help and exit"
+	echo "  -A, --Authors       show the name of all authors"
+    echo "  -v, --version       show the version and exit"
 	exit 0
 }
 
 DAY_MESSAGE(){
 	echo "	*** Quote of the day ***"
 	echo ""
-	cat /home/douglas/bin/termquots/db_quotes.txt | sort -R | tail -15 | sort -R | tail -5 | sort -R | tail -1
-	echo " "	
+	sort -R ~/bin/termquots/db_quotes.txt | tail -15 | sort -R | tail -1
+	echo " "
+    exit 0	
 }
 
 ERR_MESSAGE="
 	$(basename $0): invalid option $1
 	Try '$(basename $0) -h, --help' for more informtion.
 "
+
+VERSION=$(egrep "^# Version" ~/bin/termquots/termquots.sh | cut -d' ' -f 2)
 
 
 
@@ -43,6 +50,10 @@ then
 
 		"-A" | "--Authors") cat ~/bin/termquots/db_quotes.txt | cut -d '"' -f 3 | sort | uniq 
 			;;
+                
+        "-v" | "--version") echo "$VERSION"
+            ;;
+        
 
 		*) echo "$ERR_MESSAGE"
 	 		exit 1
